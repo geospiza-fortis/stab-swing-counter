85bf145cdbf6499d1e9218bbe6ffec29b1dd4517
geospiza <geospiza@protonmail.com>
2021-01-16 23:07:31 -0800

    Show cropped images

--- main.py
+++ main.py
@@ -15,14 +15,20 @@ while cap.isOpened():
         print("Can't receive frame (stream end?). Exiting ...")
         break
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.rectangle(
-        gray,
-        ((W // 2) - 250, (H // 2 + 50)),
-        ((W // 2) + 50, (H // 2 + 350)),
-        (0, 255, 0),
-        3,
-    )
-    cv.imshow("frame", gray)
+    upper_left = ((W // 2) - 250, (H // 2 + 50))
+    lower_right = ((W // 2) + 50, (H // 2 + 350))
+    # show the full video with a rectangle
+    # cv.rectangle(
+    #     gray,
+    #     upper_left,
+    #     lower_right,
+    #     (0, 255, 0),
+    #     3,
+    # )
+    # cv.imshow("frame", gray)
+    crop_img = gray[upper_left[1] : lower_right[1], upper_left[0] : lower_right[0]]
+    cv.imshow("cropped", crop_img)
+
     if cv.waitKey(1) == ord("q"):
         break
 
