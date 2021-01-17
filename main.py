67620eaddb51793f15d3184d89df8b92097871c3
geospiza <geospiza@protonmail.com>
2021-01-17 00:25:25 -0800

    Add some stuff with umap and hdbscan for clustering

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import click
 from pathlib import Path
 from tqdm import tqdm
 
+import matplotlib.pyplot as plt
+import umap
+import hdbscan
+import json
+
 # 1080 x 1920
 H = 1080
 W = 1920
@@ -73,6 +78,54 @@ def prepare_samples(input, output, frames):
     print("done collecting frames")
     cap.release()
     cv.destroyAllWindows()
+
+
+@cli.command()
+@click.argument("input", type=click.Path(file_okay=False, exists=True))
+@click.argument("output", type=click.Path(file_okay=False))
+def cluster(input, output):
+    output = Path(output)
+    output.mkdir(parents=True, exist_ok=True)
+
+    data = []
+    for path in tqdm(sorted(Path(input).glob("*.png"))):
+        # assume square images, lets resize for efficiency
+        img = cv.imread(str(path), cv.IMREAD_GRAYSCALE)
+        img = cv.resize(img, (64, 64))
+        data.append(img.ravel())
+    # create matrix
+    x = np.array(data)
+    print(f"created array of shape {x.shape}")
+
+    # https://umap-learn.readthedocs.io/en/latest/clustering.html#
+    # this first one is for visualization
+    standard_embedding = umap.UMAP().fit_transform(x)
+
+    clusterable_embedding = umap.UMAP(
+        n_neighbors=30, min_dist=0.0, n_components=2
+    ).fit_transform(x)
+
+    labels = hdbscan.HDBSCAN(
+        min_samples=10,
+        min_cluster_size=50,
+    ).fit_predict(clusterable_embedding)
+
+    clustered = labels >= 0
+    plt.scatter(
+        standard_embedding[~clustered, 0],
+        standard_embedding[~clustered, 1],
+        c=(0.5, 0.5, 0.5),
+        alpha=0.5,
+    )
+    plt.scatter(
+        standard_embedding[clustered, 0],
+        standard_embedding[clustered, 1],
+        c=labels[clustered],
+        cmap="Spectral",
+    )
+    plt.savefig(f"{output}/plot.png")
+    with (output / "labels.json").open("w") as fp:
+        json.dump(labels.tolist(), fp, indent=2)
 
 
 if __name__ == "__main__":
