1e6c849aab9fdd7241257936522331efab30c540
geospiza <geospiza@protonmail.com>
2021-01-18 14:21:51 -0800

    Update flavor text

--- report/src/components/Plot.svelte
+++ report/src/components/Plot.svelte
@@ -6,26 +6,36 @@
   let predData;
   let plotElement;
 
-  function transform(data) {
-    return zip(...data).map(row => ({ y: row }));
+  function transform(data, args) {
+    return zip(...data).map((row, i) => ({ y: row, ...args[i] }));
   }
 
   onMount(async () => {
     let resp = await fetch("pred.csv");
     let data = await resp.text();
     predData = Papa.parse(data).data;
-    let plot = new Plotly.newPlot(plotElement, transform(predData), {
-      xaxis: {
-        range: [0, 1200],
-        rangeselector: { visible: true },
-        rangeslider: {}
-      },
-      margin: {
-        l: 50,
-        r: 0,
-        b: 50
+    let plot = new Plotly.newPlot(
+      plotElement,
+      transform(predData, [
+        { name: "other" },
+        { name: "stab" },
+        { name: "swing" }
+      ]),
+      {
+        title: "Probability of being in an animation state",
+        xaxis: {
+          range: [0, 1200],
+          rangeselector: { visible: true },
+          rangeslider: {}
+        },
+        margin: {
+          l: 50,
+          r: 0,
+          b: 50,
+          t: 50
+        }
       }
-    });
+    );
   });
 </script>
 
