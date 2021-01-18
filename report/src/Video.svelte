38db4b91b5ecdd47cb0f8bb3bbd722727576ba72
geospiza <geospiza@protonmail.com>
2021-01-18 13:08:49 -0800

    Add video and favicon

---
+++ report/src/Video.svelte
@@ -0,0 +1,9 @@
+<script>
+  let data;
+  let currentTime = 0;
+  let paused = true;
+</script>
+
+<video src="labeled.mp4" type="video/mp4" controls bind:currentTime bind:paused>
+  <track kind="captions" />
+</video>
