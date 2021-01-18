32cb62563d71f913e852293a8a538d14cad3a806
geospiza <geospiza@protonmail.com>
2021-01-18 00:34:21 -0800

    Add plot for data

--- report/src/App.svelte
+++ report/src/App.svelte
@@ -1,5 +1,31 @@
 <script>
+  import { zip } from "lodash";
+  import Papa from "papaparse";
+  import { onMount } from "svelte";
 
+  let predData;
+  let plotElement;
+
+  function transform(data) {
+    return zip(...data).map(row => ({ y: row }));
+  }
+
+  onMount(async () => {
+    let resp = await fetch("pred.csv");
+    let data = await resp.text();
+    predData = Papa.parse(data).data;
+    let plot = new Plotly.newPlot(plotElement, transform(predData), {
+      xaxis: {
+        rangeselector: { visible: true },
+        rangeslider: {}
+      },
+      margin: {
+        l: 50,
+        r: 0,
+        b: 50
+      }
+    });
+  });
 </script>
 
 <style>
@@ -11,4 +37,8 @@
 
 <main>
   <h1>Counting swings and stabs</h1>
+
+  <div bind:this={plotElement} />
+
+  {#if predData}{JSON.stringify(transform(predData))}{/if}
 </main>
