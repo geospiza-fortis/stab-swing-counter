02a17439569bf7d82ad27af624d7de180638a4ec
geospiza <geospiza@protonmail.com>
2021-01-18 22:15:22 -0800

    Rename data to trial

--- run_copy.py
+++ run_copy.py
@@ -4,15 +4,15 @@ from pathlib import Path
 import os
 import json
 
-shutil.rmtree("report/public/data", ignore_errors=True)
-shutil.copytree("data/v1/output", "report/public/data")
-for path in Path("report/public/data").glob("**/*.mp4"):
+shutil.rmtree("report/public/trial", ignore_errors=True)
+shutil.copytree("data/v1/output", "report/public/trial")
+for path in Path("report/public/trial").glob("**/*.mp4"):
     os.remove(path)
 
 manifest = []
-for path in Path("report/public/data").glob("*"):
+for path in Path("report/public/trial").glob("*"):
     manifest.append(path.name)
-Path("report/public/data/manifest.json").write_text(json.dumps(manifest, indent=2))
+Path("report/public/trial/manifest.json").write_text(json.dumps(manifest, indent=2))
 
 
 run(
