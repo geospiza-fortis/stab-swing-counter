921c9ff1f217795efb55ccead16d43c4c36d4aaa
geospiza <geospiza@protonmail.com>
2021-01-24 02:22:17 -0800

    Add model that evaluates on a sliding window with clean signals

--- README.md
+++ README.md
@@ -1,31 +1,10 @@
 # counting frames
 
-```bash
-# watch the video, press q to quit
-python main.py watch data/input/spear.mkv
-python main.py watch data/input/spear.mkv --crop
-
-# take 30 seconds of frames into images for preprocessing
-python main.py prepare-samples data/input/spear.mkv data/output/spear_cropped/
-python main.py prepare-samples data/input/polearm.mkv data/output/polearm_cropped/
-```
-
-Made a few templates based on a small region in the cropped screenshot. I will
-run a template matching algorithm, and then pass the features into a multi-class
-classifier.
-
-```bash
-python .\main.py train-classifier .\data\labeled\spear\ data/model/spear_logistic .\data\templates\stab.png .\data\templates\swing.png
-
-python .\main.py evaluate-model .\data\output\spear_cropped\ .\data\output\spear_eval\ data/model/spear_logistic .\data\templates\stab.png .\data\templates\swing.png
-
-python .\main.py evaluate-model .\data\output\polearm_cropped\ .\data\output\polearm_eval\ data/model/spear_logistic .\data\templates\stab.png .\data\templates\swing.png
-
-python .\main.py evaluate-video .\data\input\spear.mkv .\data\output\spear_eval_video\ data/model/spear_logistic .\data\templates\stab.png .\data\templates\swing.png
-```
-
-Here's 10 trials worth of data. The API changed since the previous one, where
-I'm using multiple smaller templates and omitting the position.
+I run a template matching algorithm, and then pass the features into a
+multi-class classifier. Made a few templates based on a small region in the
+cropped screenshot for pattern matching in openCV. Here's 10 trials worth of
+data. The API changed since the previous one, where I'm using multiple smaller
+templates and omitting the position.
 
 ```powershell
 python main.py watch '.\data\v1\input\00.mkv' --crop --offset-x -175
@@ -41,4 +20,9 @@ python main.py find-template '.\data\v1\input\01.mkv' '.\data\v1\templates\name.
 python run_trials.py
 python run_compress.py
 python run_copy.py
+```
+
+```
+python main.py train-sliding-classifier data/v1/labeled data/v1/prep data/v1/model/logistic data/v1/templates
+
 ```
--- main.py
+++ main.py
@@ -278,5 +278,167 @@ def evaluate_video(input, output, model_input, templates, offset_x):
     (output / "pred.json").write_text(json.dumps(pred_label))
 
 
+@cli.command()
+@click.argument("input", type=click.Path(file_okay=False, exists=True))
+@click.argument("input-full", type=click.Path(file_okay=False, exists=True))
+@click.argument("output", type=click.Path(file_okay=False))
+@click.argument("templates", type=click.Path(file_okay=False, exists=True))
+@click.option("--window", type=int, default=8)
+def train_sliding_classifier(input, input_full, output, templates, window):
+    input = Path(input)
+    output = Path(output)
+    output.mkdir(parents=True, exist_ok=True)
+
+    template_img = []
+    for path in Path(templates).glob("*.png"):
+        template_img.append(cv.imread(str(path), cv.IMREAD_GRAYSCALE))
+
+    # find the frames we need to read into memory and cache them
+    def parse_number(name):
+        return int(name.split("_")[-1].split(".")[0].lstrip("0") or "0")
+
+    # cache frames
+    frames = {}
+    input_full_path = sorted(Path(input_full).glob("**/*.png"))
+    input_paths = sorted(Path(input).glob("**/*.png"))
+    for path in tqdm(input_paths):
+        idx = parse_number(path.name) + 1
+        if idx - window < 0:
+            raise ValueError(f"lag on frame is less than 0 - {idx} on {path.name}")
+        for frame in input_full_path[idx - window : idx]:
+            if frame.name in frames:
+                continue
+            img = cv.imread(str(frame), cv.IMREAD_GRAYSCALE)
+            feature = np.array(
+                [get_template_features(tmpl, img) for tmpl in template_img]
+            ).reshape(-1)
+            frames[frame.name] = feature
+
+    data = []
+    labels = []
+    for path in input_paths:
+        label_class = path.parent.name
+        idx = parse_number(path.name) + 1
+        row = np.array(
+            [frames[p.name] for p in input_full_path[idx - window : idx]]
+        ).reshape(-1)
+        data.append(row)
+        labels.append(label_class)
+
+    le = LabelEncoder()
+    le.fit(labels)
+    print(list(le.classes_))
+    y = le.transform(labels)
+
+    X = np.array(data)
+    print(f"data in shape {X.shape}")
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    clf = LogisticRegression().fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"accuracy: {acc} with {len(y_test)} samples")
+
+    # retrain on all the data
+    clf = LogisticRegression(max_iter=10000).fit(X, y)
+    acc = accuracy_score(y, clf.predict(X))
+    print(f"accuracy: {acc} on all data of length {len(y)}")
+
+    with (output / "model.pkl").open("wb") as fp:
+        pickle.dump(clf, fp)
+    with (output / "labels.json").open("w") as fp:
+        # for mapping label back to the original string later
+        json.dump({i: v for i, v in enumerate(le.classes_)}, fp)
+
+
+def evaluate(clf, labels, data, frames, video, output, batch_num, plot=False):
+    X = np.array(data)
+    y = clf.predict_proba(X)
+    y_label = [labels[str(p)] for p in clf.predict(X)]
+
+    for img, label in zip(frames, y_label):
+        font = cv.FONT_HERSHEY_SIMPLEX
+        img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+        cv.putText(img, label, (10, 50), font, 2, (0, 0, 0), 2, cv.LINE_AA)
+        video.write(img)
+
+    if plot:
+        plt.clf()
+        xs = list(range(len(data)))
+        for k, v in labels.items():
+            if v == "other":
+                continue
+            plt.plot(xs, y[:, int(k)], label=v)
+        plt.legend()
+        plt.savefig(f"{output}/probs_{batch_num:03}.png")
+
+    return y, y_label
+
+
+@cli.command()
+@click.argument("input", type=click.Path(file_okay=False, exists=True))
+@click.argument("output", type=click.Path(file_okay=False))
+@click.argument("model_input", type=click.Path(file_okay=False, exists=True))
+@click.argument("templates", type=click.Path(file_okay=False, exists=True))
+@click.option("--window", type=int, default=8)
+@click.option("--batch-size", type=int, default=128)
+def evaluate_sliding_model(input, output, model_input, templates, window, batch_size):
+    input = Path(input)
+    model_input = Path(model_input)
+    output = Path(output)
+    output.mkdir(parents=True, exist_ok=True)
+
+    template_img = []
+    for path in Path(templates).glob("*.png"):
+        template_img.append(cv.imread(str(path), cv.IMREAD_GRAYSCALE))
+
+    with (model_input / "model.pkl").open("rb") as fp:
+        clf = pickle.load(fp)
+    labels = json.loads((model_input / "labels.json").read_text())
+
+    fourcc = cv.VideoWriter_fourcc(*"mp4v")
+    video = cv.VideoWriter(f"{output}/labeled.mp4", fourcc, 60, (300, 300))
+
+    history = []
+    data = []
+    frames = []
+    pred_prob = []
+    pred_label = []
+    batch_num = 0
+
+    for path in tqdm(sorted(Path(input).glob("**/*.png"))):
+        if len(data) < batch_size:
+            frame = cv.imread(str(path), cv.IMREAD_GRAYSCALE)
+            feature = np.array(
+                [get_template_features(tmpl, frame) for tmpl in template_img]
+            ).reshape(-1)
+            frames.append(frame)
+            history.append(feature)
+            if len(history) < window:
+                continue
+            assert len(history) == window
+            data.append(np.array(history).reshape(-1))
+            history.pop(0)
+        else:
+            y, y_label = evaluate(
+                clf, labels, data, frames, video, output, batch_num, True
+            )
+            pred_prob.append(y)
+            pred_label.append(y_label)
+            data = []
+            frames = []
+            batch_num += 1
+
+    y, y_label = evaluate(clf, labels, data, frames, video, output, batch_num, True)
+    pred_prob.append(y)
+    pred_label.append(y_label)
+
+    np.savetxt(f"{output}/pred.csv", y, delimiter=",")
+    (output / "pred.json").write_text(json.dumps(pred_label))
+
+    cv.destroyAllWindows()
+    video.release()
+
+
 if __name__ == "__main__":
     cli()
